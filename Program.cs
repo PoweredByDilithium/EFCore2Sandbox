f4308c7e2bad78eda74774a471d60905769ef842
kyle johnson <kmjnson@gmail.com>
2018-03-24 17:14:14 +0000

    201803241714

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ComputerInventory
+{
+  class Program {
+    static void Main(string[] args) {
+      Console.WriteLine("Hello World!");
+    }
+  }
+}
